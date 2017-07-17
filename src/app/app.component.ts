79b034375c1b74b5420c5e889741c3e37c852842
Nikolay Kupriyanov <nickkupriyanov@gmail.com>
2017-07-17 12:21:06 +0300

    fixed scripts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
 import { UsaYrdService } from './shared/services/usa-yrd.service';
 import { ExtraService } from './shared/services/extra.service';
 
@@ -11,15 +10,5 @@ import { ExtraService } from './shared/services/extra.service';
 })
 export class AppComponent {
 
-    constructor(
-        private router: Router,
-        private usaYrdService: UsaYrdService,
-        private extraService: ExtraService,
-    ) {
-        this.router.events.subscribe(res => {
-            if (res instanceof NavigationEnd) {
-                this.extraService.emit(res.url)
-            }
-        })
-    }
+    constructor() { }
 }
--- src/app/extra/extra.component.ts
+++ src/app/extra/extra.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, ElementRef } from '@angular/core';
 import { ExtraService } from '../shared/services/extra.service';
 import { UsaYrdService } from '../shared/services/usa-yrd.service'
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'yrd-extra',
@@ -13,39 +14,51 @@ export class ExtraComponent implements OnInit {
         private extraService: ExtraService,
         private usaYrdService: UsaYrdService,
         private elementRef: ElementRef,
+        private router: Router,
     ) {
-        extraService.observable$.subscribe((res: string) => {
-            switch (res.replace('/', '')) {
-                case 'agency':
-                    console.log('agency')
-                    this.addScripts([
-                        'assets/3rd-scripts/agency.js',
-                    ]);
-                    break;
-                case 'contacts':
-                    console.log('contact')
-                    break;
-                case 'work':
-                    console.log('work');
-                    setTimeout(() => {
+        this.router.events.subscribe(res => {
+            if (res instanceof NavigationEnd) {
+                this.clear();
+                switch (res.url.replace('/', '')) {
+                    case 'agency':
+                        console.log('agency')
                         this.addScripts([
-                            'assets/3rd-scripts/title.js',
+                            'assets/3rd-scripts/agency.js',
                         ]);
-                    }, 1000);
-                    break;
-                case '':
-                    console.log('home')
-                    this.addScripts([
-                        'assets/3rd-scripts/anime.min.js',
-                        'assets/3rd-scripts/main.js',
-                        'assets/3rd-scripts/hire-us.js',
-                    ])
-                    break;
+                        break;
+                    case 'contacts':
+                        console.log('contact')
+                        break;
+                    case 'work':
+                        console.log('work');
+                        setTimeout(() => {
+                            this.addScripts([
+                                'assets/3rd-scripts/title.js',
+                            ]);
+                        }, 1000);
+                        break;
+                    case '':
+                        console.log('home')
+                        this.addScripts([
+                            'assets/3rd-scripts/anime.min.js',
+                            'assets/3rd-scripts/hire-us.js',
+                        ])
+                        break;
+                }
             }
-        });
+        })
     }
 
     ngOnInit() { }
+
+    clear() {
+        const children = this.elementRef.nativeElement.children;
+        console.log(children.length);
+        for (const c of children) {
+            this.elementRef.nativeElement.removeChild(c);
+        }
+        console.log(children.length);
+    }
 
     addScripts(scripts) {
         for (const script of scripts) {
